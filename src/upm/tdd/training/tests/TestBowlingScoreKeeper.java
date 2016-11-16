ca3b9a5a56ec0dd2d63696601afac89388560366
somename <someemail>
2016-11-16 17:59:44 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -10,70 +10,7 @@ import upm.tdd.training.BowlingGame;
 import upm.tdd.training.Frame;
 
 public class TestBowlingScoreKeeper {
-	private BowlingGame b;
 	
-	@Before
-	public void setUp() throws BowlingException
-	{
-		Frame.resetIdCounting();
-		
-		b = new BowlingGame();
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-	}
-	
-
-	@Test
-	public void testCorrectAddFrame() throws BowlingException {
-		Frame f = new Frame( 1, 2 );
-		b.addFrame( f  );
-		assertEquals( true, f.isLastFrame() );
-	}
-	
-	@Test(expected = BowlingException.class)
-	public void testNotCorrectAddFrame() throws BowlingException {
-		b.addFrame( new Frame( 1, 2 )  );
-		b.addFrame( new Frame( 1, 2 )  );
-	}
-	
-	@Test
-	public void testIsNextFrameBonusStrike() throws BowlingException {
-		b.addFrame( new Frame( 10, 0 )  );
-		assertEquals(true, b.isNextFrameBonus());
-	}
-	
-	@Test
-	public void testIsNextFrameBonusSpire() throws BowlingException {
-		b.addFrame( new Frame( 5, 5 )  );
-		assertEquals(true, b.isNextFrameBonus());
-	}
-	
-	@Test
-	public void testIsNotNextFrameBonus() throws BowlingException {
-		b.addFrame( new Frame( 1, 2 )  );
-		assertEquals(false, b.isNextFrameBonus());
-	}
-	//////////////////////////////////////////
-	@Test
-	public void testSetBonus() throws BowlingException {
-		b.addFrame( new Frame( 10, 0 )  );
-		b.setBonus( 5 , 5 );
-		
-	}
-	
-	@Test(expected = BowlingException.class)
-	public void testCntSetBonus() throws BowlingException {
-		b.addFrame( new Frame( 0, 0 )  );
-		b.setBonus( 5 , 5 );
-		
-	}
 	
 
 }
