99c92d7ded31c4e8860e4d483e8f8a5aadcf9db1
somename <someemail>
2016-11-16 18:06:52 +0100

    besouro automatic message

---
+++ src/upm/tdd/training/tests/AllTests.java
@@ -0,0 +1,12 @@
+package upm.tdd.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+public class AllTests {
+
+
+}
