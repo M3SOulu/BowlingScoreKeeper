0355b7469b77358470cc8d8ca7c82ac863312d9b
somename <someemail>
2016-11-16 18:08:15 +0100

    besouro automatic message

--- src/upm/tdd/training/BowlingException.java
+++ src/upm/tdd/training/BowlingException.java
@@ -7,5 +7,9 @@ public class BowlingException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public BowlingException(){
+		System.err.println("Errore");
+	}
+	
 
 }
