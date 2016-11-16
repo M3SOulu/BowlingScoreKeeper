6989eafea43450c2fe02c8672745927ae3a1e50a
somename <someemail>
2016-11-16 17:32:19 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -39,26 +39,6 @@ public class TestBowlingScoreKeeper {
 	
 	@Test
 	public void testScoreNormal() {
-		game.getFrames().get(0).setFirstThrow(2);
-		game.getFrames().get(0).setSecondThrow(2);
-		game.getFrames().get(1).setFirstThrow(1);
-		game.getFrames().get(1).setSecondThrow(2);
-		game.getFrames().get(2).setFirstThrow(1);
-		game.getFrames().get(2).setSecondThrow(2);
-		game.getFrames().get(3).setFirstThrow(4);
-		game.getFrames().get(3).setSecondThrow(3);
-		game.getFrames().get(4).setFirstThrow(0);
-		game.getFrames().get(4).setSecondThrow(5);
-		game.getFrames().get(5).setFirstThrow(6);
-		game.getFrames().get(5).setSecondThrow(0);
-		game.getFrames().get(6).setFirstThrow(1);
-		game.getFrames().get(6).setSecondThrow(1);
-		game.getFrames().get(7).setFirstThrow(0);
-		game.getFrames().get(7).setSecondThrow(0);
-		game.getFrames().get(8).setFirstThrow(0);
-		game.getFrames().get(8).setSecondThrow(0);
-		game.getFrames().get(9).setFirstThrow(1);
-		game.getFrames().get(9).setSecondThrow(2);
 		assertEquals(game.score(), 33);
 	}
 	
