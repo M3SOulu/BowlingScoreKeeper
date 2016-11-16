1e79b2d906b76631d538fa826082b431b703b15a
somename <someemail>
2016-11-16 16:39:01 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -1,11 +1,28 @@
 package upm.tdd.training.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestBowlingScoreKeeper {
+import upm.tdd.training.BowlingGame;
+import upm.tdd.training.Frame;
 
+public class TestBowlingScoreKeeper {
+	BowlingGame partita;
+	Frame frame;
+	@Before
+	public static void setUpBeforeClass() throws Exception {
+		
+		partita = new BowlingGame();
+		frame = new Frame(10,0);
+	}
+	
+	@Test
+	public void tutti_10_score_equals_100() {
+		partita.addFrame(frame);
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
