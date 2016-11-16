3e6655b3fade9a7d7b7c579d1a0e6c4d92f25e19
somename <someemail>
2016-11-16 15:45:03 +0100

    besouro automatic message

---
+++ src/upm/tdd/training/tests/TestFrame.java
@@ -0,0 +1,16 @@
+package upm.tdd.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import upm.tdd.training.Frame;
+
+public class TestFrame {
+
+	@Test
+	public void testIsStrike() {
+		assertEquals(true, new Frame( 10, 0 ) );
+	}
+
+}
