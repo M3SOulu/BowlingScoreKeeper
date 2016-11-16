9c24feb7888101255ff7767ddd750d82bba4ab1a
somename <someemail>
2016-11-16 17:53:33 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingGameException.java
+++ src/upm/tdd/training/tests/TestBowlingGameException.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import upm.tdd.training.BowlingException;
+import upm.tdd.training.Frame;
+
 public class TestBowlingGameException {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = BowlingException.class)
+	public void eleventhFrameSholudNotAccepted() throws BowlingException {
+		// Arrange
+		Frame frame11;
+		// Act
+		frame11 = new Frame(1, 4);
+		partita.addFrame(frame11);
 	}
 
 }
