2bdf588749393220bdb1238a090c87f27c965d91
somename <someemail>
2016-11-16 17:56:24 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/AllBowlingTest.java
+++ src/upm/tdd/training/tests/AllBowlingTest.java
@@ -2,13 +2,17 @@ package upm.tdd.training.tests;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(TestBowlingGameException.class,TestBowlingScoreKeeper.class)
 
 public class AllBowlingTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
+	
 
 }
