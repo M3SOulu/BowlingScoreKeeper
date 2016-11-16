df89801f68f69fe06e589edc6c6fc44d40939850
somename <someemail>
2016-11-16 17:07:50 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestFrame.java
+++ src/upm/tdd/training/tests/TestFrame.java
@@ -9,6 +9,47 @@ import upm.tdd.training.Frame;
 
 public class TestFrame {
 
+	@Test(expected = BowlingException.class)
+	public void createInvalidFrame1() throws BowlingException{
+		new Frame( 11, 0 );
+	}
+
+	@Test(expected = BowlingException.class)
+	public void createInvalidFrame2() throws BowlingException{
+		new Frame( 0, 11 );
+	}
+
+	@Test(expected = BowlingException.class)
+	public void createInvalidFrame3() throws BowlingException{
+		new Frame( 11, 11 );
+	}
+
+	@Test
+	public void createValidFrame() throws BowlingException{
+		Frame f = new Frame( 0, 0 );
+		assertEquals( false, f.isSpare() );
+	}
+
+	@Test
+	public void testIsStrike() throws BowlingException {
+		assertEquals( true, new Frame( 10, 0 ).isStrike() );
+	}
+
+	@Test
+	public void testIsNotStrike() throws BowlingException {
+		assertEquals( false, new Frame( 5, 5 ).isStrike() );
+	}
+
+	@Test
+	public void testIsSpare() throws BowlingException {
+		assertEquals( true, new Frame( 5, 5 ).isSpare() );
+	}
+
+	@Test
+	public void testIsNotSpare() throws BowlingException {
+		assertEquals( false, new Frame( 5, 4 ).isSpare() );
+	}
+
 	@Test
 	public void testIsLastFrame() throws BowlingException{
 		Frame.resetIdCounting();
@@ -23,7 +64,7 @@ public class TestFrame {
 		assertEquals( false, new Frame(0, 0).isLastFrame() );
 		assertEquals( true, new Frame(0, 0).isLastFrame() );
 	}
-	
+
 	@Test
 	public void testIsLastFrameWithSpare() throws BowlingException{
 		Frame.resetIdCounting();
@@ -39,7 +80,7 @@ public class TestFrame {
 		assertEquals( false, new Frame(5, 5).isLastFrame() );
 		assertEquals( true, new Frame(5, 5).isLastFrame() );
 	}
-	
+
 	@Test
 	public void testIsLastFrameWithStrike() throws BowlingException{
 		Frame.resetIdCounting();
@@ -55,62 +96,21 @@ public class TestFrame {
 		assertEquals( false, new Frame(10, 0).isLastFrame() );
 		assertEquals( true, new Frame(5, 5).isLastFrame() );
 	}
-	
-	@Test(expected = BowlingException.class)
-	public void createInvalidFrame1() throws BowlingException{
-		new Frame( 11, 0 );
-	}
-	
-	@Test(expected = BowlingException.class)
-	public void createInvalidFrame2() throws BowlingException{
-		new Frame( 0, 11 );
-	}
-	
-	@Test(expected = BowlingException.class)
-	public void createInvalidFrame3() throws BowlingException{
-		new Frame( 11, 11 );
-	}
-	
-	@Test
-	public void createValidFrame() throws BowlingException{
-		Frame f = new Frame( 0, 0 );
-		assertEquals( false, f.isSpare() );
-	}
-	
-	@Test
-	public void testIsStrike() throws BowlingException {
-		assertEquals( true, new Frame( 10, 0 ).isStrike() );
-	}
-	
-	@Test
-	public void testIsNotStrike() throws BowlingException {
-		assertEquals( false, new Frame( 5, 5 ).isStrike() );
-	}
-	
-	@Test
-	public void testIsSpare() throws BowlingException {
-		assertEquals( true, new Frame( 5, 5 ).isSpare() );
-	}
-	
-	@Test
-	public void testIsNotSpare() throws BowlingException {
-		assertEquals( false, new Frame( 5, 4 ).isSpare() );
-	}
-	
+
 	@Test
 	public void test2Bonus() throws BowlingException {
 		assertEquals( 2, new Frame( 10, 0 ).bonus() );
 	}
-	
+
 	@Test
 	public void test1Bonus() throws BowlingException {
 		assertEquals( 1, new Frame( 5, 5 ).bonus() );
 	}
-	
+
 	@Test
 	public void test0Bonus() throws BowlingException {
 		assertEquals( 0, new Frame( 2, 3 ).bonus() );
 	}
-	
+
 
 }
