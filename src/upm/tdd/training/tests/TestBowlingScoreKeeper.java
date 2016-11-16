ef23d4b23b93a1db54ba6fe0ef3b9b6618c7516e
somename <someemail>
2016-11-16 17:57:34 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -92,4 +92,29 @@ public class TestBowlingScoreKeeper {
 		game.addFrame(f10);
 		assertEquals(game.score(), 47);
 	}
+	
+	@Test
+	public void testScoreStrikeAndSpareAn() {
+		Frame f1 = new Frame(10,0);
+		Frame f2 = new Frame(1,9);
+		Frame f3 = new Frame(1,0);
+		Frame f4 = new Frame(4,6);
+		Frame f5 = new Frame(2,1);
+		Frame f6 = new Frame(0,0);
+		Frame f7 = new Frame(0,0);
+		Frame f8 = new Frame(0,0);
+		Frame f9 = new Frame(0,0);
+		Frame f10 = new Frame(0,0,1);
+		game.addFrame(f1);
+		game.addFrame(f2);
+		game.addFrame(f3);
+		game.addFrame(f4);
+		game.addFrame(f5);
+		game.addFrame(f6);
+		game.addFrame(f7);
+		game.addFrame(f8);
+		game.addFrame(f9);
+		game.addFrame(f10);
+		assertEquals(game.score(), 48);
+	}
 }
