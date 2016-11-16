c877191f296c468463bd878e95ba60149fd070d3
somename <someemail>
2016-11-16 16:09:24 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -39,8 +39,10 @@ public class Frame {
 
 	//bonus throws (1=true,0=false)
 	public int bonus(){
-		if(isStrike() || isSpare())
+		if(isSpare())
 			return 1;
+		else if(isStrike() || isSpare())
+			return 2;
 		return 0;
 	}
 
