f39246854f1a443d4a4d79931bd231ed81a0dcfb
somename <someemail>
2016-11-16 16:13:01 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -4,6 +4,7 @@ package upm.tdd.training;
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
+	private final int MAX_PINS = 10;
 	
 	public Frame(int firstThrow, int secondThrow){
 		this.firstThrow = firstThrow;
@@ -11,32 +12,31 @@ public class Frame {
 	}
 	
 	//the score of a single frame
-	public int score(){
-		//to be implemented
-		return 0;
+	public int score() throws BowlingException{
+		int result = firstThrow + secondThrow;
+		if (result < 0 || result > 10)
+			throw new BowlingException();
+		return result;
 	}
 
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
-		//to be implemented
-		return false;
+		return firstThrow == MAX_PINS ? true : false;
 	}
 	
 	//return whether a frame is a spare or not
 	public boolean isSpare(){
-		//to be implemented
-		return false;
+		return ((!isStrike()) && (firstThrow + secondThrow == MAX_PINS)) ? true : false;
 	}
 
 	//return whether this is the last frame of the match
 	public boolean isLastFrame(){
-		//to be implemented
+		
 		return false;
 	}
 
 	//bonus throws
 	public int bonus(){
-		//to be implemented
 		return 0;
 	}
 
