dadda98ef8dd07fc605acc58b46ac083410c423d
somename <someemail>
2016-11-16 17:47:09 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -2,17 +2,32 @@ package upm.tdd.training.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import upm.tdd.training.BowlingException;
 import upm.tdd.training.BowlingGame;
+import upm.tdd.training.Frame;
 
 public class TestBowlingScoreKeeper {
-
-	BowlingGame ciao = new BowlingGame();
-	
 		
 	@Test
-	public void test() {
+	public void test() throws BowlingException {
+		BowlingGame test = new BowlingGame();
+			
+		test.addFrame(new Frame(1, 4));
+		test.addFrame(new Frame(4, 5));
+		test.addFrame(new Frame(6, 4));
+		test.addFrame(new Frame(5, 5));
+		test.addFrame(new Frame(10, 0));
+		test.addFrame(new Frame(0, 1));
+		test.addFrame(new Frame(7, 3));
+		test.addFrame(new Frame(6, 4));
+		test.addFrame(new Frame(10, 0));
+		test.addFrame(new Frame(2, 8));
+		
 		assertEquals(133, 133);
 	}
 
