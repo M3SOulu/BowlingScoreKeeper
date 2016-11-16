0a46ec58afb5d5bc72d53451e9f1838cd0f52299
somename <someemail>
2016-11-16 17:01:06 +0100

    besouro automatic message

--- src/upm/tdd/training/BowlingGame.java
+++ src/upm/tdd/training/BowlingGame.java
@@ -1,29 +1,58 @@
 package upm.tdd.training;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BowlingGame {
-	//a bowling game is made of (at least) 10 frames
+	// a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	
-	public BowlingGame(){}
-	
-	public void addFrame(Frame frame){
-		//to be implemented
+
+	public BowlingGame() {
 	}
-	
-	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+
+	public void addFrame(Frame frame) throws BowlingException {
+		if (frame.score() > 10)
+			throw new BowlingException();
+		if (frames.size() >= 10)
+			throw new BowlingException();
+		frames.add(frame);
 	}
-	
-	public int score(){
-		//to be implemented
-		return 0;
+
+	public void setBonus(int firstThrow, int secondThrow) throws BowlingException {
+		bonus = new Frame(firstThrow, secondThrow);
 	}
-	
-	public boolean isNextFrameBonus(){
-		//to be implemented
+
+	public int score() throws BowlingException {
+		int res = 0;
+		int count = 0;
+		Frame next;
+		for (Frame f : frames) {
+			res += f.score();
+			if (f != frames.get(9)) {
+				// prendo l'elemento di posizione count e successivamente
+				// aggiorno count
+				next = frames.get(count++);
+				if (f.isStrike()) {
+					res += next.score();
+					if (next.getFirstThrow() == 10 && next != frames.get(9))
+						res += frames.get(count + 1).getFirstThrow();
+				}
+				if (f.isSpare())
+					res += next.getFirstThrow();
+			}
+		}
+		if (isNextFrameBonus())
+			res += bonus.score();
+		if (res > 300)
+			throw new BowlingException();
+		return res;
+	}
+
+	public boolean isNextFrameBonus() {
+		if (frames.size() == 10 && frames.get(9).score() == 10)
+			return true;
 		return false;
 	}
+
 }
