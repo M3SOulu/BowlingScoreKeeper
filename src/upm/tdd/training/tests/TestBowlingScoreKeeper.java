5ccf169846e03304a17129b55791fddfa2c3546a
somename <someemail>
2016-11-16 17:08:21 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -32,8 +32,9 @@ public class TestBowlingScoreKeeper {
 		partita.addFrame(frame);
 		partita.addFrame(frame);
 		partita.addFrame(frame);
+		partita.addFrame(frame);
 		
-		assertEquals(300,partita.score());
+		assertEquals(250,partita.score());
 	}
 	
 	
