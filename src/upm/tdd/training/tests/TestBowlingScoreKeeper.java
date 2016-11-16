a236305e9d882cd0eee888b646d4bf17759b5bb7
somename <someemail>
2016-11-16 17:34:06 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -8,26 +8,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestBowlingScoreKeeper {
-	private int firstThrow;
-	private int secondThrow;
+import upm.tdd.training.BowlingException;
+
+public class TestBowlingScoreKeeper{
+	private upm.tdd.training.Frame firstThrow;
+	private upm.tdd.training.Frame secondThrow;
+	private upm.tdd.training.Frame score;
+	private upm.tdd.training.Frame frame;
 	
 	@Before
 	public void SetUp(){
-		
+		frame = new upm.tdd.training.Frame(1, 4); 
 	}
 	
-	@After
-	public void Score(){
-		
-	}
-	
-	@Test
-	public void testScore(){
+	@Test(expected = Exception.class)
+	public void testScore() throws BowlingException {
 		//ARRANGE all necessary preconditions and inputs.
 		upm.tdd.training.Frame myFrame = new upm.tdd.training.Frame(1, 4);
 		//ACT on the object or method under test.
-		myFrame.score();	
+		frame.score();	
 		//ASSERT that the expected results have occurred
 		assertEquals(5, myFrame.score());
 	}
