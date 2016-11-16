690f76ab78a2b54be849fab902e5aba3d6b49fbf
somename <someemail>
2016-11-16 17:27:24 +0100

    besouro automatic message

---
+++ src/upm/tdd/training/tests/TestFrame.java
@@ -0,0 +1,31 @@
+package upm.tdd.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import upm.tdd.training.Frame;
+
+public class TestFrame {
+	Frame fr1;
+	Frame fr2;
+	Frame fr_spare;
+	Frame fr_strike;
+	Frame fr_bonus;
+
+	@Before
+	public void setUp(){
+		fr1=new Frame(5,4);
+		fr2=new Frame(7,2);
+		fr_spare=new Frame(7,3);
+		fr_strike=new Frame(10,0);
+		fr_bonus=new Frame(3,6);
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
