53bfbe63e04fdd8f9c8535aac305fb926d6aa9ba
somename <someemail>
2016-11-16 16:50:01 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -3,6 +3,7 @@ package upm.tdd.training.tests;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.junit.Assert;
 import upm.tdd.training.BowlingException;
 import upm.tdd.training.BowlingGame;
 import upm.tdd.training.Frame;
@@ -18,6 +19,9 @@ public class TestBowlingScoreKeeper {
 	@Test
 	public void testAllStrikesScore() throws BowlingException {
 		Frame strikeFrame = new Frame(10,0);
+		for(int i=0; i<10; i++) game.addFrame(strikeFrame);
+		game.setBonus(10, 10);
+		Assert.assertEquals(300, game.score());
 	}
 
 }
