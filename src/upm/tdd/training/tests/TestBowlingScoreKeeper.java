3aea39bf7b7c21ded5f24a79613eedd94ab385bf
somename <someemail>
2016-11-16 16:36:19 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -13,30 +13,14 @@ import upm.tdd.training.BowlingGame;
 public class TestBowlingScoreKeeper {
 
 	BowlingGame partita = new BowlingGame();
-	Frame frame1;
-	Frame frame2;
-	Frame frame3;
-	Frame frame4;
-	Frame frame5;
-	Frame frame6;
-	Frame frame7;
-	Frame frame8;
-	Frame frame9;
-	Frame frame10;
+
 
 	@Before
 	public void setUp() throws BowlingException {
 		// Act
-		partita.addFrame(frame1);
-		partita.addFrame(frame2);
-		partita.addFrame(frame3);
-		partita.addFrame(frame4);
-		partita.addFrame(frame5);
-		partita.addFrame(frame6);
-		partita.addFrame(frame7);
-		partita.addFrame(frame8);
-		partita.addFrame(frame9);
-		partita.addFrame(frame10);
+		for(int i=0;i<10;i++){
+			partita.addFrame(new Frame());
+		}
 
 	}
 
