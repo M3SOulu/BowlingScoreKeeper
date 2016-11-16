e7a3f390955029fb61556dc26cb9cb082a3d7863
somename <someemail>
2016-11-16 15:36:37 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -20,7 +20,7 @@ public class Frame {
 	//the score of a single frame
 	public int score(){
 		
-		return 0;
+		return (firstThrow+secondThrow);
 	}
 
 	//returns whether the frame is a strike or not
