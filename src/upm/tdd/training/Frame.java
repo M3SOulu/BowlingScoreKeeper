7c9a964c31404b1389fd00609dfa3e1e3e4374c4
somename <someemail>
2016-11-16 15:31:18 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -1,44 +1,48 @@
 package upm.tdd.training;
 
-
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
+	private int totalFrame;
 	
-	public Frame(int firstThrow, int secondThrow){
+
+	public Frame(int firstThrow, int secondThrow) {
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
 	}
-	
-	//the score of a single frame
-	public int score(){
-		//to be implemented
+
+	// the score of a single frame
+	public int score() {
+		totalFrame+=this.firstThrow;
+		return totalFrame;
+	}
+
+	// returns whether the frame is a strike or not
+	public boolean isStrike() {
+		if (this.firstThrow == 10) {
+			return true;
+		}
+		return false;
+	}
+
+	// return whether a frame is a spare or not
+	public boolean isSpare() {
+		if (this.firstThrow + this.secondThrow == 10) {
+			return true;
+		}
+		return false;
+	}
+
+	// return whether this is the last frame of the match
+	public boolean isLastFrame() {
+		// to be implemented
+		return false;
+	}
+
+	// bonus throws
+	public int bonus() {
+		// to be implemented
 		return 0;
 	}
-
-	//returns whether the frame is a strike or not
-	public boolean isStrike(){
-		//to be implemented
-		return false;
-	}
-	
-	//return whether a frame is a spare or not
-	public boolean isSpare(){
-		//to be implemented
-		return false;
-	}
-
-	//return whether this is the last frame of the match
-	public boolean isLastFrame(){
-		//to be implemented
-		return false;
-	}
-
-	//bonus throws
-	public int bonus(){
-		//to be implemented
-		return 0;
-	}
-
 
 }
