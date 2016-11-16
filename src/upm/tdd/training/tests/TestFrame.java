24f12408f1de69e15f6a9a404e6712460adcd0cf
somename <someemail>
2016-11-16 17:16:37 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestFrame.java
+++ src/upm/tdd/training/tests/TestFrame.java
@@ -62,16 +62,16 @@ public class TestFrame {
 		BowlingGame bg = new BowlingGame();
 		Frame f1 = new Frame(1,4);
 		bg.addFrame(f1);
-		Frame f2 = new Frame(4,5);
-		bg.addFrame(f2);
-		Frame f3 = new Frame(6,4);
-		bg.addFrame(f3);
-		Frame f4 = new Frame(5,5);
-		bg.addFrame(f4);
-		Frame f5 = new Frame(10,0);
-		bg.addFrame(f5);
-		Frame f6 = new Frame(0,1);
-		bg.addFrame(f6);
+//		Frame f2 = new Frame(4,5);
+//		bg.addFrame(f2);
+//		Frame f3 = new Frame(6,4);
+//		bg.addFrame(f3);
+//		Frame f4 = new Frame(5,5);
+//		bg.addFrame(f4);
+//		Frame f5 = new Frame(10,0);
+//		bg.addFrame(f5);
+//		Frame f6 = new Frame(0,1);
+//		bg.addFrame(f6);
 		assert(bg.score()==61);
 		
 	}
