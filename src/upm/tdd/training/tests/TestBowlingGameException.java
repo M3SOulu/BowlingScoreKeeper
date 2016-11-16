cf98ca0cb56505648d3b95f8e490ed57b28cb3ca
somename <someemail>
2016-11-16 17:59:20 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingGameException.java
+++ src/upm/tdd/training/tests/TestBowlingGameException.java
@@ -11,26 +11,29 @@ import upm.tdd.training.Frame;
 
 public class TestBowlingGameException {
 	BowlingGame partita = new BowlingGame();
+
 	@Before
+
 	public void setUp() throws BowlingException {
-		// Act
-		for(int i=0;i<10;i++){
-			partita.addFrame(new Frame(10,10));
-		}
 
 	}
 
 	@Test(expected = BowlingException.class)
 	public void eleventhFrameSholudNotAccepted() throws BowlingException {
+		// Act
+		for (int i = 0; i < 10; i++) {
+			partita.addFrame(new Frame(10, 10));
+		}
 		// Arrange
 		Frame frame11;
 		// Act
 		frame11 = new Frame(1, 4);
 		partita.addFrame(frame11);
 	}
-	
+
 	@Test(expected = BowlingException.class)
 	public void BowlingGameNotInitialized() throws BowlingException {
+
 		partita.score();
 	}
 
