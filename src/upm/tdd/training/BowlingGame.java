6701f08ca3b7a6e09eb8a5850ad5b58237d6319d
somename <someemail>
2016-11-16 17:09:22 +0100

    besouro automatic message

--- src/upm/tdd/training/BowlingGame.java
+++ src/upm/tdd/training/BowlingGame.java
@@ -13,7 +13,7 @@ public class BowlingGame {
 	public BowlingGame(){}
 	
 	public void addFrame(Frame frame) throws BowlingException{
-		if(frames.size() == 10){
+		if(frames.size() > 10){
 			if(frames.get(9).isStrike()){
 				setBonus(frame.getFirstThrow(),frame.getSecondThrow());
 			}
