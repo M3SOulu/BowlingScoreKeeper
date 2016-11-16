ff0594212a11f45d19d3186e18434dd6e0ba6d7c
somename <someemail>
2016-11-16 17:51:08 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -11,10 +11,9 @@ import upm.tdd.training.BowlingGame;
 import upm.tdd.training.Frame;
 
 public class TestBowlingScoreKeeper {
-
-	BowlingGame testGame = new BowlingGame();
 	
-	public void initFrames() throws BowlingException{
+	public static void initFrames() throws BowlingException{
+		BowlingGame testGame = new BowlingGame();
 		ArrayList<Frame> frames = new ArrayList<Frame>();
 		for (int i = 0; i < 10; ++i){
 			frames.add(i, new Frame (0, 0));
@@ -25,13 +24,9 @@ public class TestBowlingScoreKeeper {
 	
 	@Test
 	public void testScore() throws BowlingException{
+		initFrames();
 		int score = testGame.score();
 		assertEquals (score, 0);
-	}
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
