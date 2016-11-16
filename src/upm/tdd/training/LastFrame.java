8f4e8dcf51ff8c1ad174da5e77ac2569548c5d35
somename <someemail>
2016-11-16 16:13:02 +0100

    besouro automatic message

--- src/upm/tdd/training/LastFrame.java
+++ src/upm/tdd/training/LastFrame.java
@@ -1,5 +1,14 @@
 package upm.tdd.training;
 
-public class LastFrame {
+public class LastFrame extends Frame {
+
+	private int thirdThrow;
+	
+	public LastFrame(int firstThrow, int secondThrow, int thirdThrow) {
+		super(firstThrow, secondThrow);
+		this.thirdThrow = thirdThrow;
+	}
+	
+	
 
 }
