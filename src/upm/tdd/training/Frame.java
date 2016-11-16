5d5dacb19d4b48ab4c887d3ec7fa4a1b7917be48
somename <someemail>
2016-11-16 15:59:10 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -1,46 +1,45 @@
 package upm.tdd.training;
 
-
 public class Frame {
 	private static final int MAX_FRAME_SCORE = 10;
-	
+
 	private int firstThrow;
 	private int secondThrow;
-	
-	public Frame(int firstThrow, int secondThrow) throws BowlingException{
-		if (firstThrow < 0 || firstThrow > 10) {
+
+	public Frame(int firstThrow, int secondThrow) throws BowlingException {
+		if (areNotValidThrows()) {
 			throw new BowlingException();
+		} else {
+			this.firstThrow = firstThrow;
+			this.secondThrow = secondThrow;
 		}
-		
-		this.firstThrow = firstThrow;
-		this.secondThrow = secondThrow;
 	}
-	
-	//the score of a single frame
-	public int score(){
-		//to be implemented
+
+	// the score of a single frame
+	public int score() {
+		// to be implemented
 		return 0;
 	}
 
-	//returns whether the frame is a strike or not
-	public boolean isStrike(){
+	// returns whether the frame is a strike or not
+	public boolean isStrike() {
 		return this.firstThrow == MAX_FRAME_SCORE;
 	}
-	
-	//return whether a frame is a spare or not
-	public boolean isSpare(){
+
+	// return whether a frame is a spare or not
+	public boolean isSpare() {
 		return (this.firstThrow + this.secondThrow) == MAX_FRAME_SCORE;
 	}
 
-	//return whether this is the last frame of the match
-	public boolean isLastFrame(){
-		//to be implemented
+	// return whether this is the last frame of the match
+	public boolean isLastFrame() {
+		// to be implemented
 		return false;
 	}
 
-	//bonus throws
-	public int bonus(){
-		//to be implemented
+	// bonus throws
+	public int bonus() {
+		// to be implemented
 		return 0;
 	}
 
@@ -51,8 +50,9 @@ public class Frame {
 	public int getSecondThrow() {
 		return secondThrow;
 	}
-	
-	
 
+	private boolean areNotValidThrows() {
+		return firstThrow < 0 || firstThrow > 10 || secondThrow < 0 || secondThrow > 10;
+	}
 
 }
