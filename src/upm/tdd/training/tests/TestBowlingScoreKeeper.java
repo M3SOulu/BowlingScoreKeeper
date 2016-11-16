34b9ca5bf8d3e1b6b45f7b2151a279ace5cdf4a8
somename <someemail>
2016-11-16 17:01:16 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -5,29 +5,44 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import upm.tdd.training.BowlingException;
 import upm.tdd.training.BowlingGame;
-
+import upm.tdd.training.Frame;
 
 //@RunWith(Parameterized.class)
 public class TestBowlingScoreKeeper {
-	
-	/*@Parameters
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] { { "IIII" }, { "XXXX" }, { "CCCC" }, { "XXXX" }, { "MMMM" }, { "VV" },
-				{ "LL" }, { "DD" }, { "IIV" }, { "IIX" }, { "XXL" }, { "XXC" }, { "CCD" }, { "CCM" }
 
-		});
-	}*/
-	
+	/*
+	 * @Parameters public static Collection<Object[]> data() { return
+	 * Arrays.asList(new Object[][] { { "IIII" }, { "XXXX" }, { "CCCC" }, {
+	 * "XXXX" }, { "MMMM" }, { "VV" }, { "LL" }, { "DD" }, { "IIV" }, { "IIX" },
+	 * { "XXL" }, { "XXC" }, { "CCD" }, { "CCM" }
+	 * 
+	 * }); }
+	 */
+
+	Frame[] frame;
+	BowlingGame game;
+
+	@Before
+	public void initialize() throws BowlingException {
+		game = new BowlingGame();
+		for (int i = 0; i < 10; i++) {
+			frame[i] = new Frame(4, 5);
+		}
+	}
 
 	@Test
-	public void test() {
-		BowlingGame game = new BowlingGame();
+	public void test() throws BowlingException {
+		for (int i = 0; i < frame.length; i++) {
+			game.addFrame(frame[i]);
+		}
 	}
 
 }
