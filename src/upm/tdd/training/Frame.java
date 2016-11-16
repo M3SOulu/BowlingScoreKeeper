e61de326079f85f03e52e5ef2d0f490ae191bcf9
somename <someemail>
2016-11-16 15:21:51 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -18,8 +18,7 @@ public class Frame {
 
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
-		//to be implemented
-		return false;
+		return firstThrow == 10;
 	}
 	
 	//return whether a frame is a spare or not
