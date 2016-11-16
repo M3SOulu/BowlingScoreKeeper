0111aa41bb501a8e7d31d0a351943ee0f6b56224
somename <someemail>
2016-11-16 18:09:14 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -33,14 +33,23 @@ public class TestBowlingScoreKeeper {
 	@Test
 	public void testScoreAfterASpare() {
 		BowlingGame partita2 = new BowlingGame();
-		Frame strike = new Frame(5,5);
+		Frame spare = new Frame(5,5);
 		Frame frame1 = new Frame(5,2);
-		partita2.addFrame(strike);
+		partita2.addFrame(spare);
 		partita2.addFrame(frame1);
 	
 		assertEquals(15+7, partita2.score());
 	}
 	
-
+ public void testAScoreAfter3Strikes() {
+	 BowlingGame partita3 = new BowlingGame();
+	Frame strike = new Frame(10,0);
+	
+	partita3.addFrame(strike);
+	partita3.addFrame(strike);
+	partita3.addFrame(strike);
+		
+	assertEquals(30+20+10, partita3.score());
+ }
 	
 }
