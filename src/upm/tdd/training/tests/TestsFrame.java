2638b133dca2f803b73a8d83573617053254e5c0
somename <someemail>
2016-11-16 15:50:34 +0100

    besouro automatic message

---
+++ src/upm/tdd/training/tests/TestsFrame.java
@@ -0,0 +1,22 @@
+package upm.tdd.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import upm.tdd.training.BowlingException;
+import upm.tdd.training.Frame;
+
+public class TestsFrame {
+	
+	
+
+	@Test(expected = BowlingException.class)
+	public void testStrike() throws BowlingException {
+		//Assign
+		Frame newFrame = new Frame(10, 1);
+		
+	}
+
+}
