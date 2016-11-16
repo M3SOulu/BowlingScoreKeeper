32fe33bb0244cc8e3eff8e8def73325b0b814824
somename <someemail>
2016-11-16 15:54:35 +0100

    besouro automatic message

--- src/upm/tdd/training/BowlingGame.java
+++ src/upm/tdd/training/BowlingGame.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
-	private List<Frame> frames = new ArrayList<Frame>();
+	private List<Frame> frames = new ArrayList<Frame>(10);
 	private Frame bonus;
 	static private int Throws=0;
 	
-	public BowlingGame(){}
-	
-	public void addFrame(Frame frame){
+	public BowlingGame(){
 		
 	}
 	
-	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+	public void addFrame(Frame frame){
+		frames.add(Throws++, frame);
+	}
+	
+	public void setBonus(int firstThrow, int secondThrow){
+		
 	}
 	
 	public int score(){
