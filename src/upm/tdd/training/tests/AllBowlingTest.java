bd07553924333276731b053ee0125e4ab7172934
somename <someemail>
2016-11-16 17:56:46 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/AllBowlingTest.java
+++ src/upm/tdd/training/tests/AllBowlingTest.java
@@ -4,15 +4,13 @@ import static org.junit.Assert.*;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(TestBowlingGameException.class,TestBowlingScoreKeeper.class)
+@Suite.SuiteClasses({TestBowlingGameException.class,TestBowlingScoreKeeper.class})
 
 public class AllBowlingTest {
-
-	
-	
 
 }
