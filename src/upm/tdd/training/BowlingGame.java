7a33517ebfadd5b148b898072cbc83bcef9f2b81
somename <someemail>
2016-11-16 17:47:41 +0100

    besouro automatic message

--- src/upm/tdd/training/BowlingGame.java
+++ src/upm/tdd/training/BowlingGame.java
@@ -28,7 +28,7 @@ public class BowlingGame {
 		if(frames.size() > 1 && previous.isStrike()  && frame.isStrike() ) {
 			previous.setBonusPoints(frame.getFirstThrow());
 		} else if (frames.size() > 1 && previous.isStrike() && !frame.isStrike()) {
-			frame.setBonusPoints(frame.getFirstThrow() + frame.getSecondThrow());
+			previous.setBonusPoints(previous.getFirstThrow() + previous.getSecondThrow());
 		}
 		
 		frames.add(frame);
