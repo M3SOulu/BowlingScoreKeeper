54336cdfff063236afc06537acdf603ee5904baf
somename <someemail>
2016-11-16 17:37:13 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -1,44 +1,43 @@
 package upm.tdd.training;
 
 public class Frame {
-	
-	private static int frameCounterGlobal=0;
+
+	private static int frameCounterGlobal = 0;
 	private int firstThrow;
 	private int secondThrow;
 	private int frameNumber;
-	
-	
 
 	public Frame(int firstThrow, int secondThrow) {
-		if(this.firstThrow==10){
+		if (this.firstThrow == 10) {
 			this.firstThrow = firstThrow;
-		}else{
+		} else {
 			this.firstThrow = firstThrow;
 			this.secondThrow = secondThrow;
 		}
-		
-		frameNumber=frameCounterGlobal++;
-		
-	}
-	
-	public Frame(int firstThrow){
-		this.firstThrow=10;
-		this.secondThrow=0;
+
+		frameNumber = frameCounterGlobal++;
+
 	}
 
-	public int getFirstThrow(){
+	public Frame(int firstThrow) {
+		this.firstThrow = 10;
+		this.secondThrow = 0;
+	}
+
+	public int getFirstThrow() {
 		return this.firstThrow;
 	}
-	
-	public int getSecondThrow(){
+
+	public int getSecondThrow() {
 		return this.secondThrow;
 	}
+
 	// the score of a single frame
 	public int score() {
-		if(this.firstThrow+this.secondThrow==20){
+		if (this.firstThrow + this.secondThrow == 20) {
 			return 10;
 		}
-		return this.firstThrow+this.secondThrow;
+		return this.firstThrow + this.secondThrow;
 	}
 
 	// returns whether the frame is a strike or not
@@ -59,14 +58,14 @@ public class Frame {
 
 	// return whether this is the last frame of the match
 	public boolean isLastFrame() {
-		if(this.frameNumber==10){
+		if (this.frameNumber == 10) {
 			return true;
 		}
 		return false;
 	}
-	
-	public boolean isFirstFrame(){
-		if(this.frameNumber==0){
+
+	public boolean isFirstFrame() {
+		if (this.frameNumber == 0) {
 			return true;
 		}
 		return false;
@@ -74,7 +73,7 @@ public class Frame {
 
 	// bonus throws
 	public int bonus() {
-		
+
 		return 0;
 	}
 
