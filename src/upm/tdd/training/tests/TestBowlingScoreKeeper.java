f7caba32ecc815bf60d635ad73a08a5ee967ac7e
somename <someemail>
2016-11-16 16:52:10 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -2,9 +2,26 @@ package upm.tdd.training.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
 public class TestBowlingScoreKeeper {
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { { "IIII" }, { "XXXX" }, { "CCCC" }, { "XXXX" }, { "MMMM" }, { "VV" },
+				{ "LL" }, { "DD" }, { "IIV" }, { "IIX" }, { "XXL" }, { "XXC" }, { "CCD" }, { "CCM" }
+
+		});
+	}
+	
 
 	@Test
 	public void test() {
