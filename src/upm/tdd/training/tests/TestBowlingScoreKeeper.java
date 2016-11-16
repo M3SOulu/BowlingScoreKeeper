b659caef125980dc281e703a216001b6c8dda15a
somename <someemail>
2016-11-16 17:51:04 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -67,4 +67,29 @@ public class TestBowlingScoreKeeper {
 		game.addFrame(f10);
 		assertEquals(game.score(), 27);
 	}
+	
+	@Test
+	public void testScoreStrikeAndSpare() {
+		Frame f1 = new Frame(0,10);
+		Frame f2 = new Frame(1,9);
+		Frame f3 = new Frame(1,0);
+		Frame f4 = new Frame(4,6);
+		Frame f5 = new Frame(2,1);
+		Frame f6 = new Frame(0,0);
+		Frame f7 = new Frame(0,0);
+		Frame f8 = new Frame(0,0);
+		Frame f9 = new Frame(0,0);
+		Frame f10 = new Frame(0,0);
+		game.addFrame(f1);
+		game.addFrame(f2);
+		game.addFrame(f3);
+		game.addFrame(f4);
+		game.addFrame(f5);
+		game.addFrame(f6);
+		game.addFrame(f7);
+		game.addFrame(f8);
+		game.addFrame(f9);
+		game.addFrame(f10);
+		assertEquals(game.score(), 37);
+	}
 }
