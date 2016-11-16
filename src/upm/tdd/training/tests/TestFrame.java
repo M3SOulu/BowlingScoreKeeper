95f1785df762e3945185bbdf775d7e338630bdde
somename <someemail>
2016-11-16 16:55:55 +0100

    besouro automatic message

---
+++ src/upm/tdd/training/tests/TestFrame.java
@@ -0,0 +1,14 @@
+package upm.tdd.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFrame {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
