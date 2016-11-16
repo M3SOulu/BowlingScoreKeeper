f26063eee4a9bdbb044e29f3983b112174a01c06
somename <someemail>
2016-11-16 16:04:05 +0100

    besouro automatic message

--- src/upm/tdd/training/CannotCreateAnotherFrame.java
+++ src/upm/tdd/training/CannotCreateAnotherFrame.java
@@ -2,6 +2,11 @@ package upm.tdd.training;
 
 public class CannotCreateAnotherFrame extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	CannotCreateAnotherFrame(){
 		super("You have just created 10 Frame");
 	}
