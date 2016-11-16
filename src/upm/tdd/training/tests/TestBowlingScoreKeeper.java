58ace3dbf81b2b4b13490c598ac499d64bd8fed3
somename <someemail>
2016-11-16 18:06:35 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -25,7 +25,7 @@ public class TestBowlingScoreKeeper {
 	@Test
 	public void allStrikeInBowlingGamePoint() throws BowlingException{
 		for(int i=0;i<10;i++){
-			partita.addFrame(new Frame(10,10));
+			partita.addFrame(new Frame(10));
 			partita.setBonus(10,10);
 		}
 		assertEquals(300,partita.score());
