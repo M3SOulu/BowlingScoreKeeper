42982d752bb285620ecb98657b78f4158920a1ca
somename <someemail>
2016-11-16 15:49:21 +0100

    besouro automatic message

--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -5,7 +5,11 @@ public class Frame {
 	private int firstThrow;
 	private int secondThrow;
 	
-	public Frame( int firstThrow, int secondThrow ){
+	public Frame( int firstThrow, int secondThrow ) throws BowlingException{
+		if( firstThrow > 10 || secondThrow > 10 || (firstThrow + secondThrow) > 10 ){
+			throw new BowlingException();
+		}
+		
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
 	}
