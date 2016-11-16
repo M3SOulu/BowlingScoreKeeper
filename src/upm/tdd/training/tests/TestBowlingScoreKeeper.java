e74dd52f5daedc63361ee4cfa5631a813531ad67
somename <someemail>
2016-11-16 17:54:54 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -59,7 +59,23 @@ public class TestBowlingScoreKeeper {
 	}
 
 	@Test
-	public void dBowlingException() throws BowlingException {		
+	public void validStrikeBowlingException() throws BowlingException {		
+		// Arrange
+		Frame frame1 = new Frame(10, 0);
+		Frame frame2 = new Frame(6, 3);
+		Frame frame3 = new Frame(8, 0);
+		// Act
+		bowlingGame.addFrame(frame1);
+		bowlingGame.addFrame(frame2);
+		for (int i = 2; i < 10; i++) {
+			bowlingGame.addFrame(frame3);
+		}
+		// Assert
+		assertEquals(92, bowlingGame.score());
+	}
+	
+	@Test
+	public void validStrikeSpareConsegutiveBowlingException() throws BowlingException {		
 		// Arrange
 		Frame frame1 = new Frame(10, 0);
 		Frame frame2 = new Frame(6, 4);
@@ -71,7 +87,22 @@ public class TestBowlingScoreKeeper {
 			bowlingGame.addFrame(frame3);
 		}
 		// Assert
-		assertEquals(92, bowlingGame.score());
+		assertEquals(102, bowlingGame.score());
 	}
 
+	@Test
+	public void validSpareConsegutiveBowlingException() throws BowlingException {		
+		// Arrange
+		Frame frame1 = new Frame(7, 2);
+		Frame frame2 = new Frame(6, 4);
+		Frame frame3 = new Frame(8, 1);
+		// Act
+		bowlingGame.addFrame(frame1);
+		bowlingGame.addFrame(frame2);
+		for (int i = 2; i < 10; i++) {
+			bowlingGame.addFrame(frame3);
+		}
+		// Assert
+		assertEquals(100, bowlingGame.score());
+	}
 }
