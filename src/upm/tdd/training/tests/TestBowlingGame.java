55ced9871a43e6bb7ecd1496d55f2b94b9b0a126
somename <someemail>
2016-11-16 17:42:25 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingGame.java
+++ src/upm/tdd/training/tests/TestBowlingGame.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import upm.tdd.training.*;
+
 public class TestBowlingGame {
 
+	BowlingGame bg = new BowlingGame();
+	Frame f1 = new Frame(1,4);
+	Frame f2 = new Frame(4,5); 
+	Frame f3 = new Frame(6,4); 
+	Frame f4 = new Frame(5,5); 
+	Frame f5 = new Frame(10,0); 
+	Frame f6 = new Frame(0,1); 
+	Frame f7 = new Frame(7,3); 
+	Frame f8 = new Frame(6,4); 
+	Frame f9 = new Frame(10,0); 
+	Frame f10 = new Frame(2,8); 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddFrame() {
+		
+		
 	}
 
 }
