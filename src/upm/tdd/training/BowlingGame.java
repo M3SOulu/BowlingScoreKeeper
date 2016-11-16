8b449bfdc827424ddf8a4ff5c453a6aa917fb5d5
somename <someemail>
2016-11-16 15:55:48 +0100

    besouro automatic message

--- src/upm/tdd/training/BowlingGame.java
+++ src/upm/tdd/training/BowlingGame.java
@@ -1,29 +1,37 @@
 package upm.tdd.training;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BowlingGame {
-	//a bowling game is made of (at least) 10 frames
+
+	// a bowling game is made of (at least) 10 frames
+	private static final int MAX_FRAMES_SIZE = 10;
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	
-	public BowlingGame(){}
-	
-	public void addFrame(Frame frame){
-		frames.add(new Frame(frame.getFirstThrow(), frame.getSecondThrow()));
+
+	public BowlingGame() {
 	}
-	
+
+	public void addFrame(Frame frame) throws BowlingException {
+		if (frames.size() == MAX_FRAMES_SIZE) {
+			throw new BowlingException();
+		} else {
+			frames.add(new Frame(frame.getFirstThrow(), frame.getSecondThrow()));
+		}
+	}
+
 	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+		// to be implemented
 	}
-	
-	public int score(){
-		//to be implemented
+
+	public int score() {
+		// to be implemented
 		return 0;
 	}
-	
-	public boolean isNextFrameBonus(){
-		//to be implemented
+
+	public boolean isNextFrameBonus() {
+		// to be implemented
 		return false;
 	}
 }
--- src/upm/tdd/training/Frame.java
+++ src/upm/tdd/training/Frame.java
@@ -7,7 +7,11 @@ public class Frame {
 	private int firstThrow;
 	private int secondThrow;
 	
-	public Frame(int firstThrow, int secondThrow){
+	public Frame(int firstThrow, int secondThrow) throws BowlingException{
+		if (firstThrow < 0 || firstThrow > 10) {
+			throw new BowlingException();
+		}
+		
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
 	}
