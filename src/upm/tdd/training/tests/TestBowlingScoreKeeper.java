c5519cd5ea79ea81dc5a1a7214d9c24755ed80b4
somename <someemail>
2016-11-16 17:25:43 +0100

    besouro automatic message

--- src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
+++ src/upm/tdd/training/tests/TestBowlingScoreKeeper.java
@@ -13,31 +13,31 @@ import org.junit.After;
 public class TestBowlingScoreKeeper {
 	
 
-	BowlingGame match = new BowlingGame();
+	BowlingGame game = new BowlingGame();
 
 	@Before
 	public void inizialize() {
-		match.addFrame(new Frame(1,4));
-		match.addFrame(new Frame(4,5));
-		match.addFrame(new Frame(6,4));
-		match.addFrame(new Frame(5,5));
-		match.addFrame(new Frame(10,0));
-		match.addFrame(new Frame(0,1));
-		match.addFrame(new Frame(7,3));
-		match.addFrame(new Frame(6,4));
-		match.addFrame(new Frame(10,0));
-		match.addFrame(new Frame(2,8));
+		game.addFrame(new Frame(1,4));
+		game.addFrame(new Frame(4,5));
+		game.addFrame(new Frame(6,4));
+		game.addFrame(new Frame(5,5));
+		game.addFrame(new Frame(10,0));
+		game.addFrame(new Frame(0,1));
+		game.addFrame(new Frame(7,3));
+		game.addFrame(new Frame(6,4));
+		game.addFrame(new Frame(10,0));
+		game.addFrame(new Frame(2,8));
 	}
 	
 	@Test
 	public void test() {
-		assertTrue(match.score() == 133);
+		assertTrue(game.score() == 133);
 		fail("Not yet implemented");
 	}
 	
 	@After
-	public void setToNull(){
-		
+	public void clear(){
+		game.getFrames().clear();
 	}
 
 }
